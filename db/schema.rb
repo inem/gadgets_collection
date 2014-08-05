3bc048f84b5d914de2a92a9da369ebfb7f31a367
Ivan Nemytchenko <nemytchenko@gmail.com>
2014-08-05 09:36:43 +0200

    dragonfly gem + gadget model & migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140805071251) do
+ActiveRecord::Schema.define(version: 20140805072240) do
+
+  create_table "gadgets", force: true do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.string   "image_uid"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
