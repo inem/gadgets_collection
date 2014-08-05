a336c4137a61373098d8ab33f422e27210540fa9
Ivan Nemytchenko <nemytchenko@gmail.com>
2014-08-05 09:58:35 +0200

    gadgets controller

---
+++ app/controllers/gadgets_controller.rb
@@ -0,0 +1,34 @@
+class GadgetsController < ApplicationController
+  def show
+    @gadget = Gadget.find(params[:id])
+  end
+
+  def index
+    @gadgets = Gadget.all
+  end
+
+  def new
+    @gadget = Gadget.new
+  end
+
+  def create
+    @gadget = Gadget.create(gadget_params)
+    redirect_to gadgets_path
+  end
+
+  def edit
+    @gadget = Gadget.find(params[:id])
+  end
+
+  def update
+    @gadget = Gadget.find(params[:id])
+    @gadget.update_attributes(gadget_params)
+    redirect_to gadgets_path
+  end
+
+  private
+
+  def gadget_params
+    params.require(:gadget).permit(:image, :name)
+  end
+end
