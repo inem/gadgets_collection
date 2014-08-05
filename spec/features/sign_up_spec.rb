1e8ac1b7bb9279377076a5581a8e0036879906b1
Ivan Nemytchenko <nemytchenko@gmail.com>
2014-08-05 12:06:21 +0200

    bad but working tests

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,18 +1,28 @@
 require_relative '../rails_helper'
 
 describe "the signup process", :type => :feature do
-  # before :each do
-  #   User.make(:email => 'user@example.com', :password => 'caplin')
-  # end
+  it "signs up & signs in & signs" do
+    visit '/'
+    expect(page).to have_content 'Sign up'
+    click_link 'Sign up'
 
-  it "signs up" do
-    visit '/sessions/new'
-    within("#session") do
-      fill_in 'Email', :with => 'user@example.com'
-      fill_in 'Password', :with => 'password'
-      fill_in 'Password confirmation', :with => 'password'
-    end
+    fill_in 'Email', :with => 'user@example.com'
+    fill_in 'Password', :with => 'password'
+    fill_in 'Password confirmation', :with => 'password'
     click_button 'Sign up'
-    expect(page).to have_content 'Success'
+    expect(page).to have_content 'Sign out'
+
+
+    click_link 'Sign out'
+    expect(page).to have_content 'Sign up'
+
+
+    fill_in 'Email', :with => 'user@example.com'
+    fill_in 'Password', :with => 'password'
+    click_button 'Sign in'
+    expect(page).to have_content 'Sign out'
+
+    click_link 'Sign out'
+    expect(page).to have_content 'Sign up'
   end
 end
