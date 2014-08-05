6d434b2c155642c8aef0b0e7b969222763fc29a5
Ivan Nemytchenko <nemytchenko@gmail.com>
2014-08-05 12:21:35 +0200

    better specs

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,28 +1,55 @@
 require_relative '../rails_helper'
 
 describe "the signup process", :type => :feature do
-  it "signs up & signs in & signs" do
+  it "redirects to sign up" do
     visit '/'
     expect(page).to have_content 'Sign up'
-    click_link 'Sign up'
+  end
+
+  it "signs up" do
+    visit '/users/sign_up'
 
     fill_in 'Email', :with => 'user@example.com'
     fill_in 'Password', :with => 'password'
     fill_in 'Password confirmation', :with => 'password'
     click_button 'Sign up'
     expect(page).to have_content 'Sign out'
+  end
 
+  it "logs in" do
+    visit '/users/sign_in'
+    sign_in
 
-    click_link 'Sign out'
-    expect(page).to have_content 'Sign up'
-
-
-    fill_in 'Email', :with => 'user@example.com'
-    fill_in 'Password', :with => 'password'
-    click_button 'Sign in'
     expect(page).to have_content 'Sign out'
+  end
+
+  it "see navigation" do
+    visit '/users/sign_in'
+    sign_in
+
+    expect(page).to have_content 'List'
+    expect(page).to have_content 'Cover flow'
+    expect(page).to have_content 'New gadget'
+  end
+
+
+  it "logs out" do
+    visit '/users/sign_in'
+    sign_in
 
     click_link 'Sign out'
     expect(page).to have_content 'Sign up'
   end
+end
+
+def sign_in(user = nil)
+  unless user
+    email = 'user@example.com'
+    password = 'password'
+    user = User.create(email: email, password: password)
+  end
+
+  fill_in 'Email', :with => user.email
+  fill_in 'Password', :with => user.password
+  click_button 'Sign in'
 end
